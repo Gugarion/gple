69b9dda1c75c73478f393f2f0ad97097b213d032
Gugarion <gugarion@gmail.com>
2025-05-04 11:41:41 -0700

    Reverting to base without gh-pages support.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/GPLE/',
+    publicPath: '/',
   },
   mode: 'development',
   module: {
